5a4643143d64ab20f235d27ec58879e727394aab
Adon Osimo <ajosimo88@gmail.com>
2026-07-13 13:56:09 -0600

    Updated Sim_data to remove ex_data errors

--- R/sim_data.R
+++ R/sim_data.R
@@ -2,9 +2,9 @@
 #'
 #' @description Data set for simulation
 #'
-#' @format A list of response (y), 3 exposures (Z), binary modifier (modifier), and 14 covariates (X)
+#' @format A dataframe of response (y), 3 exposures (Z), binary modifier (modifier), and 14 covariates (X)
 #'
 #' @source Liu, J. Z., Deng, W., Lee, J., Lin, P. i D., Valeri, L., Christiani, D. C., Coull, B. A. (2021). A Cross-Validated Ensemble Approach to Robust Hypothesis Testing of Continuous Nonlinear Interactions: Application to Nutrition-Environment Studies. Journal of the American Statistical Association, 117(538), 561-573. https://doi.org/10.1080/01621459.2021.1962889
 #'
-#' @examples data(ex_data) # lazy load
-"ex_data"
+#' @examples data(Liu_data) # lazy load
+"Liu_data"
